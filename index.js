0cce54eff15af7cddc839d35f33671be4226f5f5
Krzysztof <krisaczi@yahoo.com>
2024-05-10 08:19:05 +0200

    done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const btn = document.querySelector(".btn");
 const conversion = document.querySelector(".conversion");
 const input = document.querySelector(".inputValue");
 
+//Currencies dropdown creation
 const currDrop = () => {
   currencies.forEach((element) => {
     const option = document.createElement("option");
@@ -15,13 +16,19 @@ const currDrop = () => {
 };
 currDrop();
 
+//Conversion
 btn.addEventListener("click", () => {
-  fetch(URL + selectOptions.value)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(selectOptions.value);
-      console.log(data.rates[0].mid);
-      console.log(data);
-      conversion.innerHTML = `For ${selectOptions.value} ${input.value}  you will get PLN ${(data.rates[0].mid * input.value).toFixed(2)}`;
-    });
+  try {
+    fetch(URL + selectOptions.value)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!isNaN(input.value) && input.value > 0) {
+          conversion.innerHTML = `For ${selectOptions.value} ${input.value}  you will get PLN ${(data.rates[0].mid * input.value).toFixed(2)}`;
+        } else {
+          alert("Please provide a valid amount");
+        }
+      });
+  } catch (error) {
+    alert("Calculation failed" + error);
+  }
 });
